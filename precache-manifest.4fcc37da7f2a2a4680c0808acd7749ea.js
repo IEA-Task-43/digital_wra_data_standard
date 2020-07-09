1b52fbe76b12f1822af317a9e138aa9453958375
flrs-edf <flrs-edf@users.noreply.github.com>
2020-07-09 20:50:08 +0000

    deploy: 3cd5913810ffd92fb187170c740051f6629bbdd4

---
+++ precache-manifest.4fcc37da7f2a2a4680c0808acd7749ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac7fabcb2b386d6a673f274697b1fd17",
+    "url": "/jsonforms-react-seed/index.html"
+  },
+  {
+    "revision": "f62c11b140a407ed8333",
+    "url": "/jsonforms-react-seed/static/css/main.0e04eb83.chunk.css"
+  },
+  {
+    "revision": "0edda8835c9bcf53dfca",
+    "url": "/jsonforms-react-seed/static/js/2.133a68c4.chunk.js"
+  },
+  {
+    "revision": "f62c11b140a407ed8333",
+    "url": "/jsonforms-react-seed/static/js/main.25137378.chunk.js"
+  },
+  {
+    "revision": "20df896e1dca41dbd84b",
+    "url": "/jsonforms-react-seed/static/js/runtime-main.bb604be4.js"
+  }
+]);
