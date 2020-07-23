f28d8169290ce6d71e3c30e88d45313d2e3a9ddf
flrs-edf <flrs-edf@users.noreply.github.com>
2020-07-23 16:17:02 +0000

    deploy: b7aa74d493a397b037dd52be099e604cff376071

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/digital_wra_data_standard/precache-manifest.ed2a1c4714fb0f4f6cfba416b1de20ec.js"
+  "/digital_wra_data_standard/precache-manifest.7faed88a5bb5746567f25f94e4639124.js"
 );
 
 self.addEventListener('message', (event) => {
