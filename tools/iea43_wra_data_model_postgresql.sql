4b1d2f062da7fbb1b8f83e6abedf1a0cd99e8d7c
unknown <stephen@brightwindanalysis.com>
2022-10-27 15:29:20 +0100

    iss #159 add echo_intensity to measurement_type

--- tools/iea43_wra_data_model_postgresql.sql
+++ tools/iea43_wra_data_model_postgresql.sql
@@ -134,6 +134,7 @@ INSERT INTO measurement_type (id) VALUES
     ('quality'),
     ('carrier_to_noise_ratio'),
     ('doppler_spectral_broadening'),
+    ('echo_intensity'),
     ('motion_corrected_wind_speed'),
     ('motion_corrected_wind_direction'),
     ('motion_corrected_vertical_wind_speed'),
