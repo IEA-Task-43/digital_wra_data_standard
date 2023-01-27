dca79a292a24bf94a773f2b853129bc178c52419
stephenholleran <stephenholleran@users.noreply.github.com>
2023-01-27 12:48:09 +0000

    deploy: 22384a16efd21e11eddd6419c99f5a3bbfb292fc

--- precache-manifest.31efcd064a843c5c63083999e4e61ce6.js
+++ precache-manifest.31efcd064a843c5c63083999e4e61ce6.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c09fac5a09e29a6ac5918015d13b5ef9",
+    "revision": "38af3861d9565214dca1740bea0b58b2",
     "url": "/digital_wra_data_standard/index.html"
   },
   {
-    "revision": "6e9c7a7f6c46064df4b1",
+    "revision": "0008f54463132f18604e",
     "url": "/digital_wra_data_standard/static/css/main.0e04eb83.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/digital_wra_data_standard/static/js/2.133a68c4.chunk.js"
   },
   {
-    "revision": "6e9c7a7f6c46064df4b1",
-    "url": "/digital_wra_data_standard/static/js/main.73978a07.chunk.js"
+    "revision": "0008f54463132f18604e",
+    "url": "/digital_wra_data_standard/static/js/main.c11b1b2d.chunk.js"
   },
   {
     "revision": "b122ce7f3b427e9298c9",
