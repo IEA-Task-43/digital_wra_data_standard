6a17f182302d85cf8d7b4c3bdece876e98329f65
Gibson <gibson.kersting@rwe.com>
2026-05-19 07:30:02 -0500

    iss #274 add leaf_wetness_sensor and leaf_surface_wetness

--- tools/iea43_wra_data_model_postgresql.sql
+++ tools/iea43_wra_data_model_postgresql.sql
@@ -210,6 +210,7 @@ INSERT INTO measurement_type (id) VALUES
     ('speed_of_sound_in_air'),
     ('speed_of_sound_in_water'),
     ('mass_concentration'),
+    ('leaf_surface_wetness'),
     ('timestamp'),
     ('obukhov_length'),
     ('other');
@@ -336,6 +337,7 @@ INSERT INTO sensor_type (id) VALUES
     ('rain_gauge'),
     ('ice_detection_sensor'),
     ('fog_sensor'),
+    ('leaf_wetness_sensor'),
     ('gps'),
     ('illuminance_sensor'),
     ('compass'),
